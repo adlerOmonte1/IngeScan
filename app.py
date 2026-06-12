15f922f9c1897208c05cb1ecd2fc00836e82cd59
adlerOmonte1 <klin.forever24@gmail.com>
2026-06-12 17:13:33 -0500

    Mejora de UX/UI

--- app.py
+++ app.py
@@ -392,6 +392,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host="0.0.0.0", port=port, debug=debug)
